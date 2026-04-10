ac71bb254a34310760a836a827943c48c5d5ba23
EVV1E <fabillo552@gmail.com>
2026-04-11 00:21:41 +0200

    Add more crosshair cursors

--- src/main/java/dev/evvie/waylandcraft/CursorShape.java
+++ src/main/java/dev/evvie/waylandcraft/CursorShape.java
@@ -6,6 +6,11 @@ public enum CursorShape {
 	
 	HIDE(0),
 	DEFAULT(1),
+	HELP(3),
+	POINTER(4),
+	WAIT(6),
+	TEXT(9),
+	VERTICAL_TEXT(10),
 	E_RESIZE(18),
 	N_RESIZE(19),
 	NE_RESIZE(20),
@@ -19,7 +24,10 @@ public enum CursorShape {
 	NESW_RESIZE(28),
 	NWSE_RESIZE(29),
 	COL_RESIZE(30),
-	ROW_RESIZE(31);
+	ROW_RESIZE(31),
+	ZOOM_IN(33),
+	ZOOM_OUT(34),
+	ALL_RESIZE(36);
 	
 	// Serialization number for cursor shape. Should match cursor-shape wayland protocol for all cursors except extensions.
 	public final int id;
--- src/main/java/dev/evvie/waylandcraft/mixin/GuiMixin.java
+++ src/main/java/dev/evvie/waylandcraft/mixin/GuiMixin.java
@@ -19,6 +19,15 @@ public class GuiMixin {
 	private static final ResourceLocation LEFT_RIGHT_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/left_right");
 	private static final ResourceLocation TOP_BOTTOM_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/top_bottom");
 	
+	private static final ResourceLocation HELP_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/help");
+	private static final ResourceLocation MOVE_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/move");
+	private static final ResourceLocation POINTER_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/pointer");
+	private static final ResourceLocation TEXT_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/text");
+	private static final ResourceLocation VTEXT_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/vtext");
+	private static final ResourceLocation WAIT_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/wait");
+	private static final ResourceLocation ZOOM_IN_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/zoom_in");
+	private static final ResourceLocation ZOOM_OUT_CROSSHAIR = new ResourceLocation(WaylandCraft.MOD_ID, "crosshair/zoom_out");
+	
 	@Redirect(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V", ordinal = 0))
 	public void crosshairBlitSprite(GuiGraphics context, ResourceLocation original, int x, int y, int width, int height) {
 		CursorShape cursor = WaylandCraft.instance.cursorShape;
@@ -32,8 +41,13 @@ public class GuiMixin {
 		if(cursor == null) return null;
 		
 		switch(cursor) {
-		case DEFAULT: return null;
 		case HIDE: return null;
+		case DEFAULT: return null;
+		case HELP: return HELP_CROSSHAIR;
+		case POINTER: return POINTER_CROSSHAIR;
+		case WAIT: return WAIT_CROSSHAIR;
+		case TEXT: return TEXT_CROSSHAIR;
+		case VERTICAL_TEXT: return VTEXT_CROSSHAIR;
 		case E_RESIZE: return LEFT_RIGHT_CROSSHAIR;
 		case N_RESIZE: return TOP_BOTTOM_CROSSHAIR;
 		case NE_RESIZE: return TRBL_DIAGONAL_CROSSHAIR;
@@ -48,6 +62,9 @@ public class GuiMixin {
 		case NWSE_RESIZE: return TLBR_DIAGONAL_CROSSHAIR;
 		case COL_RESIZE: return LEFT_RIGHT_CROSSHAIR;
 		case ROW_RESIZE: return TOP_BOTTOM_CROSSHAIR;
+		case ZOOM_IN: return ZOOM_IN_CROSSHAIR;
+		case ZOOM_OUT: return ZOOM_OUT_CROSSHAIR;
+		case ALL_RESIZE: return MOVE_CROSSHAIR;
 		default: return null;
 		}
 	}
