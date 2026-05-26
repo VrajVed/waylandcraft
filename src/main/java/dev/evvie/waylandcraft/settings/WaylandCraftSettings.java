052d3dc527d509d05d53ae8507bec1eeedc7c5ff
Evelyn <73487868+EVV1E@users.noreply.github.com>
2026-05-26 15:47:51 +0200

    Add settings loading and settings screen

---
+++ src/main/java/dev/evvie/waylandcraft/settings/WaylandCraftSettings.java
@@ -0,0 +1,75 @@
+package dev.evvie.waylandcraft.settings;
+
+import java.lang.reflect.Field;
+
+import org.jetbrains.annotations.Nullable;
+
+import dev.evvie.waylandcraft.WaylandCraft;
+
+/* Settings for waylandcraft
+ * 
+ * All of the fields here that do not have the "transient" modifier are written to the settings.json!
+ * Because of that and because these fields are also accessed using reflection, just don't change their names,
+ * unless you also update all the references and are okay with user settings breaking across updates.
+ */
+public class WaylandCraftSettings {
+	
+	/* The settings fields shouldn't have a modifier so that they're package-private.
+	 * This avoids code directly setting the values.
+	 */
+	
+	int pixelsPerBlock = 500;
+	
+	/* This is where the getters go */
+	
+	public int getPixelsPerBlock() {
+		return pixelsPerBlock;
+	}
+	
+	/* Methods to modifiy settings by name */
+	
+	protected void setIntSetting(String name, int value) {
+		try {
+			Field field = WaylandCraftSettings.class.getDeclaredField(name);
+			field.setInt(this, value);
+		} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+			WaylandCraft.LOGGER.error("Invalid setting accessed: '" + name + "' as int!");
+			e.printStackTrace();
+		}
+	}
+	
+	protected void setBooleanSetting(String name, boolean value) {
+		try {
+			Field field = WaylandCraftSettings.class.getDeclaredField(name);
+			field.setBoolean(this, value);
+		} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+			WaylandCraft.LOGGER.error("Invalid setting accessed: '" + name + "' as boolean!");
+			e.printStackTrace();
+		}
+	}
+	
+	// Get int setting. Returns null only when setting was not found.
+	protected @Nullable Integer getIntSetting(String name) {
+		try {
+			Field field = WaylandCraftSettings.class.getDeclaredField(name);
+			return field.getInt(this);
+		} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+			WaylandCraft.LOGGER.error("Invalid setting accessed: '" + name + "' as int!");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	// Get boolean setting. Returns null only when setting was not found.
+	protected @Nullable Boolean getBooleanSetting(String name) {
+		try {
+			Field field = WaylandCraftSettings.class.getDeclaredField(name);
+			return field.getBoolean(this);
+		} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+			WaylandCraft.LOGGER.error("Invalid setting accessed: '" + name + "' as boolean!");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
